2cf51a5a25f969ec9a5f1e1d7eeaaf85e6b1872d
CGonzalez97 <cgl76490@gmail.com>
2020-11-23 13:06:52 +0100

    Empezando login y Routes

--- trabajoMinitwitter/src/app/app.module.ts
+++ trabajoMinitwitter/src/app/app.module.ts
@@ -4,10 +4,20 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginComponentComponent } from './modelos/login-component/login-component.component';
+import { RouterModule, Routes } from '@angular/router';//Importamos Routes
+
+
+//Rutas para la navegacion
+const appRoutes: Routes = [
+  {path: 'auth/login', component: LoginComponentComponent}
+];
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponentComponent
   ],
   imports: [
     BrowserModule,
