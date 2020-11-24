02a786b04e8e8ef3b5b6960841ef5839541f2ebb
CGonzalez97 <cgl76490@gmail.com>
2020-11-24 19:44:38 +0100

    Problema de credenciales en repo arreglado

--- trabajoMinitwitter/src/app/interfaces/respuestaRegistro.ts
+++ trabajoMinitwitter/src/app/interfaces/respuestaRegistro.ts
@@ -8,12 +8,3 @@ export interface RespuestaRegistro{
     active: boolean;    
 }
 
-/*{
-  "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7InVzZXJuYW1lIjoiY2FybGlueW9zIiwiZW1haWwiOiJjZ2w3NjQ5MEBnbWFpbC5jb20iLCJwYXNzd29yZCI6IjcxMTBlZGE0ZDA5ZTA2MmFhNWU0YTM5MGIwYTU3MmFjMGQyYzAyMjAiLCJyb2xlIjoidXNlciIsImFjdGl2ZSI6ZmFsc2UsIm5ld1Bhc3N3b3JkIjoiIiwiZGVzY3JpcGNpb24iOiIiLCJ3ZWJzaXRlIjoiIiwicGhvdG9VcmwiOiIiLCJhY3RpdmF0aW9uQ29kZSI6NDM1NDUsImNyZWF0ZWQiOiIyMDIwLTExLTI0VDE2OjUyOjA5Ljg2MVoiLCJpZCI6MTg5NX0sImV4cCI6MTYwNjg0MTUzMCwiaWF0IjoxNjA2MjM2NzI5fQ.0Z222h9yrwWzSrGAAxNaFln-k2m0nvMrXxSVmC6j22s",
-  "username": "carlinyos",
-  "email": "cgl76490@gmail.com",
-  "role": "user",
-  "photoUrl": "",
-  "created": "2020-11-24T16:52:09.861Z",
-  "active": false
-}*/
