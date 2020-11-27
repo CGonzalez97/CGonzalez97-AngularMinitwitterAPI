8cd5952210937f9e84e770d8ff2f27d43da8d487
CGonzalez97 <cgl76490@gmail.com>
2020-11-27 12:42:15 +0100

    fauivaif

--- trabajoMinitwitter/src/app/modelos/login.dto.ts
+++ trabajoMinitwitter/src/app/modelos/login.dto.ts
@@ -2,5 +2,7 @@ export class LoginDto {
     email:string;
     password:string;
     constructor(email: string, password: string) {
+        //this.email = email;                 //sin esto funciona
+        //this.password = password;           //sin esto funciona
     }
 }
