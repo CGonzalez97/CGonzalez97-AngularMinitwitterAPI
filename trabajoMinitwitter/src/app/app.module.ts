7eecdae4bde312e03c6c65caf370a390e8d125e3
CGonzalez97 <cgl76490@gmail.com>
2020-11-30 12:05:38 +0100

    Estilos de listar hechos

--- trabajoMinitwitter/src/app/app.module.ts
+++ trabajoMinitwitter/src/app/app.module.ts
@@ -2,6 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import {MatInputModule} from '@angular/material/input';
 
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
@@ -35,6 +37,8 @@ const appRoutes: Routes = [
     RouterModule.forRoot(appRoutes),
     FlexLayoutModule,
     MatButtonModule,
+    MatCardModule,
+    MatInputModule,
     FormsModule,
     HttpClientModule,
     AppRoutingModule,
